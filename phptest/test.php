2da1b28e9c0bd0bcf6461af6b029fb0367472591
mahmoud <mahmoud12597@gmail.com>
2018-03-04 20:40:38 +0200

    phptest

---
+++ phptest/test.php
@@ -0,0 +1,47 @@
+<?php
+    
+    function buildInvertedIndex($filenames)
+    {
+        $invertedIndex = [];
+        
+        foreach($filenames as $filename)
+        {
+            $data = file_get_contents($filename);
+            
+            if($data === false) die('Unable to read file: ' . $filename);
+            
+            preg_match_all('/(\w+)/', $data, $matches, PREG_SET_ORDER);
+            
+            foreach($matches as $match)
+            {
+                $word = strtolower($match[0]);
+                
+                if(!array_key_exists($word, $invertedIndex)) $invertedIndex[$word] = [];
+                if(!in_array($filename, $invertedIndex[$word], true)) $invertedIndex[$word][] = $filename;
+            }
+        }
+        
+        return $invertedIndex;
+    }
+
+function lookupWord($invertedIndex, $word)
+{
+    return array_key_exists($word, $invertedIndex) ? $invertedIndex[$word] : false;
+}
+
+$invertedIndex = buildInvertedIndex(['file1.txt','file2.txt']);
+    
+    foreach(['Thank', 'NOTE', 'circumstance', 'TERMS'] as $word)
+    {
+        $word = strtolower($word);
+        $matches = lookupWord($invertedIndex, $word);
+        if($matches !== false)
+        {
+            echo "<p>Found the word \"$word\" in the following files: " . implode(', ', $matches) . "</p>";
+        }
+        else
+        {
+            echo "<p>Unable to find the word \"$word\" in the index</p>";
+        }
+    }
+?>
