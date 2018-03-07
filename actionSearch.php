7ef513d58999101b3d19f04b063f199cf0309277
Adham Khalilieh <Adham1011@github.com>
2018-03-07 15:34:37 +0200

    Ajax prepare

---
+++ actionSearch.php
@@ -0,0 +1,16 @@
+<?php
+
+
+
+
+echo '<div class="col-xs-12 col-md-7 col-md-offset-2"><a href="#"><h3>';
+echo "Title</h3></a><p>";
+echo "some text for desctiption,some text for desctiption,some text for desctiption,some text for desctiption,some text for desctiption,some text for desctiption,";
+echo "</p></div>";
+
+
+echo '<div class="col-xs-12 col-md-7 col-md-offset-2"><a href="#"><h3>';
+echo "Title</h3></a><p>";
+echo "some text for desctiption,some text for desctiption,some text for desctiption,some text for desctiption,some text for desctiption,some text for desctiption,";
+echo "</p></div>";
+?>
--- include/main.js
+++ include/main.js
@@ -3,3 +3,23 @@ $(document).ready(function(){
     $(".sign-in").slideToggle("fast")
   });
 });
+
+
+$(function(){
+  $("#search-post").submit(function(){
+    var search = $("#search").val().toLowerCase();
+    var dataString = 'search=' + search;
+    console.log(dataString);
+
+    $.ajax({
+      type: "POST",
+      url:  "actionSearch.php",
+      data: dataString,
+      cache:  true,
+      success: function(data){
+        $("#result").html(data);
+      }
+    });
+    return false;
+  });
+});
